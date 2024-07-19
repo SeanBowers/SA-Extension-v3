cdffd78124e10b83b993e69d337a3aaa701ba173
Sean Bowers <seanbowers14@gmail.com>
2024-07-19 09:27:40 -0500

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -2,24 +2,14 @@ chrome.devtools.network.onRequestFinished.addListener((request) => {
     if (request.request.url.includes("spacealpha.net")) {
       request.getContent((body, encoding) => {
         const contentType = request.response.content.mimeType;
-  
         if (contentType.includes("application/json") && isJSON(body)) {
-          try {
             const parsedBody = JSON.parse(body);
-  
-            // Process and store the responses based on the URL
             if (request.request.url.includes("/api/allOwnedPlanets")) {
-
               chrome.storage.local.set({ allOwnedPlanets: parsedBody.planets }, () => {
-
                 chrome.storage.local.get("allOwnedPlanets", (data) => {
-
                 });
               });
             }
-          } catch (error) {
-          }
-        } else {
         }
       });
     }
